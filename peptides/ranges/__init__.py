0586c78aacfd8478acd9fa543363d40009fad026
Karl Knechtel <karl.a.knechtel@gmail.com>
2022-01-11 20:35:26 -0500

    helper class to represent the repeating patterns

--- peptides/ranges/__init__.py
+++ peptides/ranges/__init__.py
@@ -1,3 +1,56 @@
+def _gcd(x, y):
+    x, y = max(x, y), min(x, y)
+    while y != 0:
+        x, y = y, x % y
+    return x
+
+
+def _lcm(x, y):
+    return (x * y) // _gcd(x, y)
+
+
+class _Pattern:
+    def __init__(self, count, sequence):
+        self._count = count
+        self._sequence = sequence
+
+
+    @staticmethod
+    def from_pattern_value(value):
+        assert isinstance(value, int) and value > 1
+        size = value.bit_length() - 1
+        return _Pattern(size, value - (1 << size))
+
+
+    @staticmethod
+    def from_string(s):
+        return _Pattern(len(s), int(s[::-1], 2))
+
+
+    def __repr__(self):
+        return f'_Pattern.from_string({repr(str(self))})'
+
+
+    def __str__(self):
+        bits = bin(self._sequence)[2:]
+        return ((self._count - len(bits)) * '0' + bits)[::-1]
+
+
+    def _padded_to(self, size):
+        assert size % self._count == 0
+        return sum(
+            self._sequence << i
+            for i in range(0, size, self._count)
+        )
+
+
+    def __and__(self, other):
+        if not isinstance(other, _Pattern):
+            return NotImplemented
+        size = _lcm(self._count, other._count)
+        return _Pattern(size, self._padded_to(size) & other._padded_to(size))
+
+
 class Range:
     """Replacement for builtin `range` that can represent unbounded ranges
     and sequences with fancier patterns."""
