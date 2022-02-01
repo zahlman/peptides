c45246c3009445c40ce37a90d4fa4e3c36fa3d6f
Karl Knechtel <karl.a.knechtel@gmail.com>
2022-02-01 17:04:08 -0500

    More methods

--- peptides/random/__init__.py
+++ peptides/random/__init__.py
@@ -1,4 +1,6 @@
 from abc import ABC, abstractmethod
+from collections.abc import Mapping
+from math import ldexp
 import sys
 
 
@@ -16,6 +18,95 @@ class Generator(ABC):
         """Return the specified number of random bytes."""
         # It doesn't really matter which byte order we use for this.
         return self.bits(count * 8).to_bytes(count, byteorder=byteorder)
+
+
+    def decimal(self):
+        """Return a random floating-point value in [0.0, 1.0).
+        All possible floats are used, but the PDF is uniform."""
+        # Adaptedrom the documentation.
+        mantissa = 0x10_0000_0000_0000 | self.bits(52)
+        exponent = -53
+        x = 0
+        while not x:
+            x = self.bits(32)
+            exponent += x.bit_length() - 32
+        return ldexp(mantissa, exponent)
+
+
+    def between(self, start, end):
+        """Return a random value from `start` to `end`.
+        The end point is included if `start` and `end` are both integers;
+        otherwise, it depends on rounding behaviour."""
+        make_int = isinstance(start, int) and isinstance(end, int)
+        size = end - start
+        if make_int:
+            size = (size if end >= start else -size) + 1
+        raw = self.decimal() * size
+        if make_int:
+            raw = int(raw) if end >= start else -int(raw)
+        return start + raw
+
+
+    def choose(
+        self, iterable, count=1, *,
+        replace=None, base_weight=1, weights=(), cumulative=False
+    ):
+        """Choose multiple values from the input.
+        `iterable` -> an iterable of values (not necessarily a sequence),
+        or a mapping from values to (integer) counts. E.g. `{'x': 4, 'y': 2}`
+        is equivalent to `['x', 'x', 'x', 'x', 'y', 'y']`.
+        `count` -> the number of elements to choose.
+        `replace` -> whether to choose with replacement. Required if `count`>1.
+        weights -> a sequence of weights to use for the first however many
+        elements of the iterable. Must be empty when `iterable` is a mapping.
+        cumulative -> if set, the values in `weights` are treated as cumulative.
+        Has no effect when `iterable` is a mapping.
+        base_weight -> the relative weight of elements beyond len(weights).
+        Has no effect when `iterable` is a mapping.
+
+        The values are returned in the order of appearance in the original
+        `iterable`. This cannot be used to make a shuffled copy.
+        """
+        raise NotImplementedError # TODO
+
+
+    def sample(
+        self, iterable, count=1, *,
+        base_weight=1, weights=(), cumulative=False
+    ):
+        """Wrapper for `choose` without replacement."""
+        return self.choose(
+            iterable, count, replace=False,
+            base_weight=base_weight, weights=weights, cumulative=cumulative
+        )
+
+
+    def values(
+        self, iterable, count=1, *,
+        base_weight=1, weights=(), cumulative=False
+    ):
+        """Wrapper for `choose` with replacement."""
+        return self.choose(
+            iterable, count, replace=True,
+            base_weight=base_weight, weights=weights, cumulative=cumulative
+        )
+
+
+    def shuffle(self, a_list):
+        """Shuffle a list in-place."""
+        raise NotImplementedError
+
+
+    def shuffled(self, iterable):
+        """Get a shuffled copy of a sequence, shuffled output of an iterator,
+        or shuffled data corresponding to a mapping (as described for `choose`).
+        """
+        if isinstance(iterable, Mapping):
+            result = [k for k, v in iterable.items() for _ in range(v)]
+        else:
+            result = list(iterable)
+        self.shuffle(result)
+        return result
 
 
 class Random(Generator):
@@ -96,3 +187,49 @@ def bits(amount):
 
 def data(amount, byteorder=sys.byteorder):
     return Random._implementation.data(amount, byteorder)
+
+
+def decimal():
+    return Random._implementation.decimal()
+
+
+def between(start, end):
+    return Random._implementation.between(start, end)
+
+
+def choose(
+    iterable, count=1, *,
+    replace=None, base_weight=1, weights=(), cumulative=False
+):
+    return Random._implementation.choose(
+        iterable, count, replace=replace,
+        base_weight=base_weight, weights=weights, cumulative=cumulative
+    )
+
+
+def sample(
+    iterable, count=1, *,
+    base_weight=1, weights=(), cumulative=False
+):
+    return Random._implementation.sample(
+        iterable, count,
+        base_weight=base_weight, weights=weights, cumulative=cumulative
+    )
+
+
+def values(
+    iterable, count=1, *,
+    base_weight=1, weights=(), cumulative=False
+):
+    return Random._implementation.values(
+        iterable, count,
+        base_weight=base_weight, weights=weights, cumulative=cumulative
+    )
+
+
+def shuffle(a_list):
+    return Random._implementation.shuffle(a_list)
+
+
+def shuffled(iterable):
+    return Random._implementation.shuffled(iterable)
