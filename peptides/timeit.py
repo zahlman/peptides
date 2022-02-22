ab4df4c293089975f036095a23430576b957e6b3
Karl Knechtel <karl.a.knechtel@gmail.com>
2022-02-21 22:27:47 -0500

    Update docstring to be more explicit and better organized

--- peptides/timeit.py
+++ peptides/timeit.py
@@ -2,9 +2,10 @@
 
 """Tool for measuring execution time of small code snippets.
 
-This module avoids a number of common traps for measuring execution
-times.  See also Tim Peters' introduction to the Algorithms chapter in
-the Python Cookbook, published by O'Reilly.
+This module attempts to minimize overhead associated with the timing loop,
+and avoid common traps in interpreting the results. The overhead is still
+non-zero, and the code here doesn't try to hide that when reporting results.
+To measure the timing overhead, try invoking the program without arguments.
 
 Library usage: see the Timer class.
 
@@ -15,7 +16,7 @@ Options:
   -n/--number N: how many times to execute 'statement' (default: see below)
   -r/--repeat N: how many times to repeat the timer (default 5)
   -s/--setup S: statement to be executed once initially (default 'pass').
-                Execution time of this setup statement is NOT timed.
+                Execution of this setup statement is NOT timed.
   -p/--process: use time.process_time() (default is time.perf_counter())
   -v/--verbose: print raw timing results; repeat for more digits precision
   -u/--unit: set the output time unit (nsec, usec, msec, or sec)
@@ -23,19 +24,13 @@ Options:
   --: separate options from statement, use when statement starts with -
   statement: statement to be timed (default 'pass')
 
-A multi-line statement may be given by specifying each line as a
-separate argument; indented lines are possible by enclosing an
-argument in quotes and using leading spaces.  Multiple -s options are
-treated similarly.
+A multi-line statement may be given by specifying each line as a separate
+argument; indented lines are possible by enclosing an argument in quotes and
+using leading spaces. Multiple -s options are treated similarly.
 
 If -n is not given, a suitable number of loops is calculated by trying
 increasing numbers from the sequence 1, 2, 5, 10, 20, 50, ... until the
 total time is at least 0.2 seconds.
-
-Note: there is a certain baseline overhead associated with executing a
-pass statement.  It differs between versions.  The code here doesn't try
-to hide it, but you should be aware of it.  The baseline overhead can be
-measured by invoking the program without arguments.
 
 Classes:
 
@@ -46,7 +41,6 @@ Functions:
     timeit(string, string) -> float
     repeat(string, string) -> list
     default_timer() -> float
-
 """
 
 
